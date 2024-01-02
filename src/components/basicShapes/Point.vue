7e78a044c2149a94b1874b9ad19a0209d8712e46
Gilmar Andrade <gilmar-andrade@outlook.com>
2024-01-02 14:19:35 -0300

    feat: Point side panel

---
+++ src/components/basicShapes/Point.vue
@@ -0,0 +1,12 @@
+<template>
+    <circle :cx="props.cx" :cy="props.cy" :r="props.r" :fill="props.fill" />
+</template>
+<script setup lang="ts">
+const props = defineProps({
+  cx: { type: Number, required: true },
+  cy: { type: Number, required: true },
+  r: { type: Number, required: true },
+  fill: { type: String, required: true },
+})
+
+</script>
--- src/views/SVGView.vue
+++ src/views/SVGView.vue
@@ -7,17 +7,10 @@
             :viewBox="`${viewportX} ${viewportY} ${viewportWidth} ${viewportHeight}`"
         >
             <!-- Origin coordinate system-->
-            <circle cx="0" cy="y" r="2" fill="gray" />
+            <circle cx="0" cy="0" r="2" fill="gray" />
 
-            <rect
-                x="10"
-                y="10"
-                width="30"
-                height="30"
-                stroke="black"
-                fill="transparent"
-                stroke-width="5"
-            />
+            <Point :cx="x" :cy="y" :r="10" :fill="`black`"/>
+
             <rect
                 x="60"
                 y="10"
@@ -77,98 +70,118 @@
                 stroke-width="5"
             />
 
-            <circle :cx="x" :cy="y" r="5" fill="orange" />
         </svg>
         <div class="panel">
-            <div class="form-group">
-                <label for="width">width</label>
-                <input id="width" type="number" v-model="width" required min="0"/>
+            <div class="section">
+                <h1>Screen</h1>
+                <div class="form-group">
+                    <label for="width">width</label>
+                    <input
+                        id="width"
+                        type="number"
+                        v-model="width"
+                        required
+                        min="0"
+                    />
+                </div>
+                <div class="form-group">
+                    <label for="height">height</label>
+                    <input
+                        id="height"
+                        type="number"
+                        v-model="height"
+                        required
+                        min="0"
+                    />
+                </div>
+                <div class="form-group">
+                    <label for="aspectRatio">aspectRatio</label>
+                    <input
+                        id="aspectRatio"
+                        type="number"
+                        v-model="aspectRatio"
+                        required
+                        disabled
+                    />
+                </div>
+                <div class="form-group">
+                    <label for="zoom">zoom (%)</label>
+                    <input
+                        id="zoom"
+                        type="number"
+                        v-model="zoom"
+                        required
+                        min="1"
+                    />
+                </div>
+                <div class="form-group">
+                    <label for="viewportWidth">viewportWidth</label>
+                    <input
+                        id="viewportWidth"
+                        type="number"
+                        v-model="viewportWidth"
+                        required
+                        disabled
+                    />
+                </div>
+                <div class="form-group">
+                    <label for="viewportHeight">viewportHeight</label>
+                    <input
+                        id="viewportHeight"
+                        type="number"
+                        v-model="viewportHeight"
+                        required
+                        disabled
+                    />
+                </div>
+                <div class="form-group">
+                    <label for="viewportX">viewportX</label>
+                    <input
+                        id="viewportX"
+                        type="number"
+                        v-model="viewportX"
+                        required
+                    />
+                </div>
+                <div class="form-group">
+                    <label for="viewportY">viewportY</label>
+                    <input
+                        id="viewportY"
+                        type="number"
+                        v-model="viewportY"
+                        required
+                    />
+                </div>
             </div>
-            <div class="form-group">
-                <label for="height">height</label>
-                <input id="height" type="number" v-model="height" required min="0"/>
+
+            <div class="section">
+                <h1>Point</h1>
+                <div class="form-group">
+                    <label for="x">x</label>
+                    <input id="x" type="number" v-model="x" required />
+                </div>
+                <div class="form-group">
+                    <label for="y">y</label>
+                    <input id="y" type="number" v-model="y" required />
+                </div>
             </div>
-            <div class="form-group">
-                <label for="aspectRatio">aspectRatio</label>
-                <input
-                    id="aspectRatio"
-                    type="number"
-                    v-model="aspectRatio"
-                    required
-                    disabled
-                />
-            </div>
-            <div class="form-group">
-                <label for="zoom">zoom (%)</label>
-                <input
-                    id="zoom"
-                    type="number"
-                    v-model="zoom"
-                    required
-                    min="1"
-                />
-            </div>
-            <div class="form-group">
-                <label for="viewportWidth">viewportWidth</label>
-                <input
-                    id="viewportWidth"
-                    type="number"
-                    v-model="viewportWidth"
-                    required
-                    disabled
-                />
-            </div>
-            <div class="form-group">
-                <label for="viewportHeight">viewportHeight</label>
-                <input
-                    id="viewportHeight"
-                    type="number"
-                    v-model="viewportHeight"
-                    required
-                    disabled
-                />
-            </div>
-            <div class="form-group">
-                <label for="viewportX">viewportX</label>
-                <input
-                    id="viewportX"
-                    type="number"
-                    v-model="viewportX"
-                    required
-                />
-            </div>
-            <div class="form-group">
-                <label for="viewportY">viewportY</label>
-                <input
-                    id="viewportY"
-                    type="number"
-                    v-model="viewportY"
-                    required
-                />
-            </div>
-            <div class="form-group">
-                <label for="x">x</label>
-                <input id="x" type="number" v-model="x" required />
-            </div>
-            <div class="form-group">
-                <label for="y">y</label>
-                <input id="y" type="number" v-model="y" required />
-            </div>
+
         </div>
     </div>
 </template>
 <script setup lang="ts">
 import { computed, ref } from "vue";
+import Point from '../components/basicShapes/Point.vue'
 
 const width = ref(300);
 const height = ref(400);
 const aspectRatio = computed(() => (width.value / height.value).toFixed(2));
 
-const viewportWidth = computed(() => width.value * 1/zoom.value*100)
-const viewportHeight = computed(() => height.value * 1/zoom.value*100)
+const viewportWidth = computed(() => ((width.value * 1) / zoom.value) * 100);
+const viewportHeight = computed(() => ((height.value * 1) / zoom.value) * 100);
 const viewportX = ref(0);
 const viewportY = ref(0);
-const zoom = ref(100)
+const zoom = ref(100);
 
 const x = ref(10);
 const y = ref(20);
@@ -190,9 +203,14 @@ svg {
     width: 300px;
 }
 
+.panel .section {
+    border: 1px solid red;
+    padding: 20px;
+}
+
 .form-group {
     display: flex;
-    padding: 20px;
+    padding: 20px 0;
     gap: 10px;
     justify-content: space-around;
     align-items: stretch;
