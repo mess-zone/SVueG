3bbfcc022d28f14374934857204e2da715c59516
Gilmar Andrade <gilmar-andrade@outlook.com>
2024-01-03 12:09:37 -0300

    feat: TreeNode class

---
+++ src/helpers/TreeNode.ts
@@ -0,0 +1,47 @@
+export class TreeNode<T> {
+    value: T;
+    children: TreeNode<T>[];
+  
+    constructor(value: T) {
+      this.value = value;
+      this.children = [];
+    }
+  
+    addChild(child: TreeNode<T>): void {
+      this.children.push(child);
+    }
+  
+    removeChild(node: TreeNode<T>): void {
+      this.children = this.children.filter((child) => child !== node);
+    }
+  
+    traverse(): void {
+      console.log(this.value);
+      this.children.forEach((child) => child.traverse());
+    }
+  }
+  
+  // Example usage:
+  // const root = new TreeNode<string>('Root');
+  
+  // const child1 = new TreeNode<string>('Child 1');
+  // const child2 = new TreeNode<string>('Child 2');
+  
+  // const subChild1 = new TreeNode<string>('Subchild 1');
+  // const subChild2 = new TreeNode<string>('Subchild 2');
+  
+  // child1.addChild(subChild1);
+  // child1.addChild(subChild2);
+  
+  // root.addChild(child1);
+  // root.addChild(child2);
+  
+  // console.log('Original tree:');
+  // root.traverse();
+  
+  // // Removing a node
+  // root.removeChild(child1);
+  
+  // console.log('\nTree after removing Child 1:');
+  // root.traverse();
+  
