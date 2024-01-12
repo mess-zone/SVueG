e8c6d74165c488f3e37456e68aa75380d14c1a90
Gilmar Andrade <gilmar-andrade@outlook.com>
2024-01-12 20:13:49 -0300

    refactor: PropertyInputNumber component onChange

--- src/components/PropertyInputNumber.vue
+++ src/components/PropertyInputNumber.vue
@@ -2,16 +2,12 @@
     <input 
         ref="inputRef" 
         type="number" 
-        :value="props.modelValue" 
-        @blur="blurHandle" 
-        @keydown.up="arrowHandle" 
-        @keydown.down="arrowHandle"
-        @keydown.enter="enterHandle" 
+        :value="props.modelValue"
     />
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
 
 const props = defineProps(['modelValue'])
 const emit = defineEmits(['update:modelValue'])
@@ -20,25 +16,34 @@ const inputRef = ref()
 
 function validate(event: Event) {
     if(inputRef.value.validity.valid) {
-            emit('update:modelValue', (event.target as HTMLInputElement).value)
+        emit('update:modelValue', Number((event.target as HTMLInputElement).value))
     } else {
         inputRef.value.value =  props.modelValue
     }
 }
 
-function blurHandle(event: Event) {
-    validate(event)
-}
+// @blur="blurHandle" 
+// @keydown.up="arrowHandle" 
+// @keydown.down="arrowHandle"
+// @keydown.enter="enterHandle" 
 
-function arrowHandle(event: Event) {
-    validate(event)
-}
+// function blurHandle(event: Event) {
+//     validate(event)
+// }
 
-function enterHandle(event: Event) {
-    validate(event)
+// function arrowHandle(event: Event) {
+//     validate(event)
+// }
 
-    inputRef.value.blur()
-}
+// function enterHandle(event: Event) {
+//     validate(event)
+
+//     inputRef.value.blur()
+// }
+
+onMounted(() => {
+    inputRef.value.addEventListener('change', validate)
+})
 
 </script>
 
