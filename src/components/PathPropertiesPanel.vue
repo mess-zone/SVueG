e10a3a81680c97fa5924d0b909c685e2254f4f46
Gilmar Andrade <gilmar-andrade@outlook.com>
2024-01-12 14:55:44 -0300

    feat: pathShape change position properties of lines

--- src/components/PathPropertiesPanel.vue
+++ src/components/PathPropertiesPanel.vue
@@ -1,6 +1,24 @@
 <template>
     <div class="section">
-        <h1>Path</h1>
+        <h1>Path</h1> {{ boundingBox }}
+        <div class="form-group">
+            <label for="positionX">x</label>
+            <input
+                id="positionX"
+                type="number"
+                v-model="positionXInput"
+                required
+            />
+        </div>
+        <div class="form-group">
+            <label for="positionY">y</label>
+            <input
+                id="positionY"
+                type="number"
+                v-model="positionYInput"
+                required
+            />
+        </div>
         <div class="form-group">
             <label for="pathRotation">rotation</label>
             <input
@@ -13,12 +31,55 @@
     </div>
 </template>
 <script setup lang="ts">
+import { useSVGBounding } from "@/composables/useSVGBounding";
 import type { NodeShapeI, PathShape } from "@/types";
+import { computed, ref } from "vue";
 
 interface Props {
     node: NodeShapeI;
 }
 
 const { node } = defineProps<Props>();
-const pathShape = node as PathShape
+const pathShape = ref(node as PathShape)
+
+const { boundingBox } = useSVGBounding(pathShape)
+
+const positionXInput = computed({
+    get() {
+        return boundingBox.x
+    },
+    set(newValue) {
+        const delta = newValue - boundingBox.x
+        pathShape.value.commands
+            .filter(c => ['M', 'L', 'H'].includes(c.letter))
+            .forEach(c => {
+                if(c.args) {
+                    c.args[0] += delta
+                }
+            })
+    }
+})
+
+const positionYInput = computed({
+    get() {
+        return boundingBox.y
+    },
+    set(newValue) {
+        const delta = newValue - boundingBox.y
+        pathShape.value.commands
+            .filter(c => ['V'].includes(c.letter))
+            .forEach(c => {
+                if(c.args) {
+                    c.args[0] += delta
+                }
+            })
+        pathShape.value.commands
+            .filter(c => ['M', 'L'].includes(c.letter))
+            .forEach(c => {
+                if(c.args) {
+                    c.args[1] += delta
+                }
+            })
+    }
+})
 </script>
