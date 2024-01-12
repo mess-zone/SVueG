a20f8220a90695ee81063df2bf89dc49b79df0bd
Gilmar Andrade <gilmar-andrade@outlook.com>
2024-01-11 21:07:04 -0300

    feat: polylineShape change position properties

--- src/components/PolylinePropertiesPanel.vue
+++ src/components/PolylinePropertiesPanel.vue
@@ -2,6 +2,24 @@
     <div class="section">
         <h1>Polyline</h1>
         <div class="form-group">
+            <label for="positionX">x</label>
+            <input
+                id="positionX"
+                type="number"
+                v-model="positionXInput"
+                required
+            />
+        </div>
+        <div class="form-group">
+            <label for="positionY">y</label>
+            <input
+                id="positionY"
+                type="number"
+                v-model="positionYInput"
+                required
+            />
+        </div>
+        <div class="form-group">
             <label for="lineRotation">rotation</label>
             <input
                 id="lineRotation"
@@ -13,7 +31,9 @@
     </div>
 </template>
 <script setup lang="ts">
+import { useSVGBounding } from "@/composables/useSVGBounding";
 import type { NodeShapeI, PolylineShape } from '@/types';
+import { computed, ref } from 'vue';
 
 interface Props {
   node: NodeShapeI
@@ -21,6 +41,28 @@ interface Props {
 
 const { node } = defineProps<Props>()
 
-const polylineShape = node as PolylineShape
+const polylineShape = ref(node as PolylineShape)
+
+const { boundingBox } = useSVGBounding(polylineShape)
+
+const positionXInput = computed({
+    get() {
+        return boundingBox.x
+    },
+    set(newValue) {
+        const delta = newValue - boundingBox.x
+        polylineShape.value.points.forEach(p => { p.x += delta })
+    }
+})
+
+const positionYInput = computed({
+    get() {
+        return boundingBox.y
+    },
+    set(newValue) {
+        const delta = newValue - boundingBox.y
+        polylineShape.value.points.forEach(p => { p.y += delta })
+    }
+})
 
 </script>
